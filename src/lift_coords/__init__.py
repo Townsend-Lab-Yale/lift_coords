d629f6a580cdb691e6e64981cc591b25869dcd64
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-07-12 15:43:40 +0100

    Add logger factory.

--- src/lift_coords/__init__.py
+++ src/lift_coords/__init__.py
@@ -14,3 +14,30 @@ except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
     del version, PackageNotFoundError
+
+# Non-pyscaffold additions
+import os
+import logging
+from typing import Union
+
+
+def _create_logger(log_level: Union[str, None] = None):
+    if log_level is None:
+        log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
+    log_level_int = getattr(logging, log_level)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(log_level_int)
+    formatter = logging.Formatter(
+        fmt='%(asctime)s - %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    ch.setFormatter(formatter)
+
+    _logger = logging.getLogger(__name__)
+    _logger.addHandler(ch)
+    _logger.setLevel(log_level_int)
+    return _logger
+
+
+logger = _create_logger()
