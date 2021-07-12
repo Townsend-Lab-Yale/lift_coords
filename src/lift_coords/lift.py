513fd12a2e2aebff7567299e6239af845d6843b4
Stephen Gaffney <stephen.gaffney@yale.edu>
2021-07-12 17:28:40 +0100

    Simplify with generic lift_over function.

--- src/lift_coords/lift.py
+++ src/lift_coords/lift.py
@@ -11,22 +11,65 @@ from .admin import copy_initial_data
 
 
 __author__ = 'Stephen Gaffney'
-
-CHAIN_HG38_19 = 'hg38_to_hg19.chain'
-CHAIN_37_HG38 = 'GRCh37_to_hg38.chain'
-CHAIN_37_19 = 'GRCh37ToHg19.chain'
-CHAIN_19_37 = 'hg19_to_GRCh37.chain'
 _logger = logging.getLogger(__name__)
+VALID_BUILDS = frozenset(['grch37', 'grch38', 'hg19', 'hg38'])
+_CHAIN_HG38_HG19 = 'hg38_to_hg19.chain'
+_CHAIN_37_HG38 = 'GRCh37_to_hg38.chain'
+_CHAIN_37_HG19 = 'GRCh37ToHg19.chain'
+_CHAIN_HG19_37 = 'hg19_to_GRCh37.chain'
+_CHAIN_38_37 = 'GRCh38_to_GRCh37.chain'
+_CHAIN_37_38 = 'GRCh37_to_GRCh38.chain'
+
+_CHAIN_LISTS = {
+    ('hg38', 'grch37'): [_CHAIN_HG38_HG19, _CHAIN_HG19_37],
+    ('grch37', 'hg19'): [_CHAIN_37_HG19],
+    ('grch38', 'grch37'): [_CHAIN_38_37],
+    ('grch37', 'grch38'): [_CHAIN_37_38],
+}
 
 
-def lift_with_chains(df, keep_orig=False, chain_list=None,
-                     new_build_name=None):
+def lift_over(df, build_in: str, build_out: str, keep_orig=False):
+    """Lift table of site info from one build to another.
+
+    Args:
+        df (pd.DataFrame): table that includes chrom + position coordinates
+        build_in: Name of input build. See VALID_BUILDS for options.
+        build_out: Name of output build.
+        keep_orig (bool): whether to keep initial coordinate columns as {col}_orig.
+
+    Returns:
+        new_table (pd.DataFrame), inds_unlifted
+    """
+    if build_in not in VALID_BUILDS or build_out not in VALID_BUILDS:
+        raise BuildArgumentException(f"Build must be one of {VALID_BUILDS}.")
+    new_table, inds_unlifted = _lift_with_chains(
+        df, keep_orig=keep_orig,
+        chain_list=_CHAIN_LISTS[(build_in, build_out)], new_build_name='GRCh37')
+    return new_table, inds_unlifted
+
+
+def _prettify_build_str(build: str) -> str:
+    """Get build string suited to MAF Build column.
+
+    >>> _prettify_build_str('grch37')
+    'GRCh37'
+
+    >>> _prettify_build_str('hg19')
+    'hg19'
+    """
+    if build.startswith('grch'):
+        return build.replace('grch', 'GRCh')
+    return build
+
+
+def _lift_with_chains(df, keep_orig=False, chain_list=None,
+                      new_build_name=None):
     """Lift in two steps, using two chain files (chain1, and chain2).
 
     Args:
         df (pd.DataFrame): data table that includes chrom + position coordinates
         keep_orig (bool): whether to keep initial coordinate columns as {col}_orig
-        chain_list (list): chain file basenames, e.g. [CHAIN_HG38_19,]
+        chain_list (list): chain file basenames, e.g. [_CHAIN_HG38_HG19,]
         new_build_name (str): OPTIONAL. name to go in new Build column
             (if input table has a build column)
     """
@@ -91,24 +134,6 @@ def lift_with_chains(df, keep_orig=False, chain_list=None,
     return df2, inds_unlifted
 
 
-def lift_38_to_grch37(df, keep_orig=False):
-    """Convert hg38 to grch37."""
-
-    df2, inds_unlifted = lift_with_chains(
-        df, keep_orig=keep_orig,
-        chain_list=[CHAIN_HG38_19, CHAIN_19_37], new_build_name='GRCh37')
-    return df2, inds_unlifted
-
-
-def lift_37_to_hg19(df, keep_orig=False):
-    """Convert hg38 to grch37."""
-
-    df2, inds_unlifted = lift_with_chains(
-        df, keep_orig=keep_orig,
-        chain_list=[CHAIN_37_19], new_build_name='hg19')
-    return df2, inds_unlifted
-
-
 def _lift_bed(bed_in_path, chain_path=None, out_dir=None):
     """Lifts ."""
 
@@ -163,5 +188,8 @@ def _match_column_str(val=None, df=None):
     else:
         return None
 
+
+class BuildArgumentException(Exception):
+    pass
 
 copy_initial_data()
